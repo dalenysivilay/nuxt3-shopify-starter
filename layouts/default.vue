ede2298cbc415b74de1416a3062ddfa785942c95
Daleny Sivilay <hi@daleny.dev>
2024-01-09 18:47:48 -0600

    configured to use layouts and shopify client

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="lg:py-8 md:py-6 sm:py-4">
+    <slot />
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,22 @@
 export default defineNuxtConfig({
-  modules: ['@vueuse/nuxt', '@pinia/nuxt', '@pinia-plugin-persistedstate/nuxt', '@unocss/nuxt'],
+  modules: [
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    '@pinia-plugin-persistedstate/nuxt',
+    '@nuxtjs/apollo',
+    '@unocss/nuxt',
+  ],
+  apollo: {
+    clients: {
+      default: {
+        httpEndpoint: process.env.SHOPIFY_STOREFRONT_URL as string,
+        httpLinkOptions: {
+          headers: {
+            'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_STOREFRONT_TOKEN as string,
+          },
+        },
+      },
+    },
+  },
   devtools: { enabled: true },
 })
