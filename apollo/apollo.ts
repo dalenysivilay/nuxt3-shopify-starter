d764a2915c6c9c6ce878e481796141cc3530ecee
Daleny Sivilay <hi@daleny.dev>
2024-01-11 00:27:53 -0600

    refactored apollo client

--- apollo/apollo.ts
+++ apollo/apollo.ts
@@ -1,14 +1,32 @@
 import process from 'node:process'
 import type { ModuleOptions } from '@nuxtjs/apollo'
 
+function shopifyConfig() {
+  const {
+    SHOPIFY_STOREFRONT_URL,
+    SHOPIFY_API_VERSION,
+    SHOPIFY_STOREFRONT_TOKEN,
+  } = process.env
+
+  if (!SHOPIFY_STOREFRONT_URL || !SHOPIFY_API_VERSION || !SHOPIFY_STOREFRONT_TOKEN)
+    throw new Error('Missing Shopify environment variables.')
+
+  const url = `https://${SHOPIFY_STOREFRONT_URL}/api/${SHOPIFY_API_VERSION}/graphql.json`
+  const headers = {
+    'X-Shopify-Storefront-Access-Token': SHOPIFY_STOREFRONT_TOKEN,
+  }
+
+  return { url, headers }
+}
+
+const { url, headers } = shopifyConfig()
+
 export const apollo: ModuleOptions = {
   clients: {
     default: {
-      httpEndpoint: process.env.SHOPIFY_STOREFRONT_URL as string,
+      httpEndpoint: url,
       httpLinkOptions: {
-        headers: {
-          'X-Shopify-Storefront-Access-Token': process.env.SHOPIFY_STOREFRONT_TOKEN as string,
-        },
+        headers,
       },
     },
   },
