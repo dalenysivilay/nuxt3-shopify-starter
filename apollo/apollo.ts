3157970d42c65cece599f17edc5e126a13d2a7f6
Daleny Sivilay <hi@daleny.dev>
2024-01-12 22:17:05 -0600

    renamed apollo config

--- apollo/apollo.ts
+++ apollo/apollo.ts
@@ -3,19 +3,18 @@ import type { ModuleOptions } from '@nuxtjs/apollo'
 
 function shopifyConfig() {
   const {
-    SHOPIFY_STOREFRONT_URL,
+    SHOPIFY_BASE_URL,
     SHOPIFY_API_VERSION,
-    SHOPIFY_STOREFRONT_TOKEN,
+    SHOPIFY_STOREFRONT_ACCESS_TOKEN,
   } = process.env
 
-  if (!SHOPIFY_STOREFRONT_URL || !SHOPIFY_API_VERSION || !SHOPIFY_STOREFRONT_TOKEN)
+  if (!SHOPIFY_BASE_URL || !SHOPIFY_API_VERSION || !SHOPIFY_STOREFRONT_ACCESS_TOKEN)
     throw new Error('Missing Shopify environment variables.')
 
-  const url = `https://${SHOPIFY_STOREFRONT_URL}/api/${SHOPIFY_API_VERSION}/graphql.json`
+  const url = `https://${SHOPIFY_BASE_URL}/api/${SHOPIFY_API_VERSION}/graphql.json`
   const headers = {
-    'X-Shopify-Storefront-Access-Token': SHOPIFY_STOREFRONT_TOKEN,
+    'X-Shopify-Storefront-Access-Token': SHOPIFY_STOREFRONT_ACCESS_TOKEN,
   }
-
   return { url, headers }
 }
 
